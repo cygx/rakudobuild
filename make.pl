df6141e2eb143fdda781448638f8e6db60169693
cygx <cygx@cpan.org>
2019-08-10 15:15:21 +0200

    second commit

--- make.pl
+++ make.pl
@@ -1,6 +1,28 @@
 use v5.12;
 use warnings;
 
+my %targets;
+my %rules;
+
+sub spurt {
+    my ($name, $contents) = @_;
+    open my $fh, '>', $name or die $!;
+    syswrite $fh, $contents or die $!;
+    close $fh;
+}
+
+sub spawn {
+    if ($^O eq 'MSWin32') {
+        system 1, @_;
+    }
+    else {
+        my $pid = fork // return -1;
+        return $pid if $pid;
+        exec @_;
+        die $!;
+    }
+}
+
 sub run {
     my ($cmd, @args) = @_;
     say join ' ', @_;
@@ -14,38 +36,72 @@ sub for_repos {
 
     while (<$fh>) {
         chomp;
-        my ($dir, $url) = split /\s+/, $_, 2;
+        my ($url, $dir) = split /\s+/, $_, 2;
         $_->($dir, $url) for @_;
     }
 
     close $fh;
 }
 
-sub init {
+sub gen {
+    my ($dest, $src, @actions) = @_;
+    $rules{$dest} = [ [$src], [@actions] ];
+}
+
+sub target {
+    my ($name, $action) = @_;
+    $targets{$name} = $action;
+}
+
+sub dispatch {
+    for (@_) {
+        die "unknown target `$_´"
+            unless exists $targets{$_};
+
+        $targets{$_}->();
+    }
+}
+
+target '--help' => sub {
+    say <<'DONE';
+TODO
+DONE
+};
+
+target init => sub {
+    spurt 'repo.list', <<'DONE' unless -f 'repo.list';
+https://github.com/MoarVM/MoarVM.git            moar
+https://github.com/perl6/nqp.git                nqp
+https://github.com/rakudo/rakudo.git            rakudo
+https://github.com/MoarVM/libatomic_ops.git     3rdparty/libatomicops
+https://github.com/libuv/libuv.git              3rdparty/libuv
+https://github.com/MoarVM/dyncall.git           3rdparty/dyncall
+https://github.com/MoarVM/dynasm.git            3rdparty/dynasm
+https://github.com/MoarVM/libtommath            3rdparty/libtommath
+https://github.com/MoarVM/cmp.git               3rdparty/cmp
+DONE
+};
+
+target update => sub {
+    my %procs;
+
     for_repos sub {
         my ($dir, $url) = @_;
-        run 'git',  'clone', '--depth=1', $url, $dir;
+
+        my $pid = spawn -e $dir
+            ? ('git', '-C', $dir, 'pull', '--depth=1')
+            : ('git',  'clone', '--depth=1', $url, $dir);
+
+        die $! if $pid <= 0;
+        $procs{$pid} = 1;
     };
-}
 
-sub update {
-    for_repos sub {
-        my ($dir, $url) = @_;
-        run 'git', '-C', $dir, 'pull', '--depth=1';
-    };
-}
+    delete $procs{(wait)} while %procs;
+};
 
-my %targets = (
-    init => \&init,
-    update => \&update,
-);
+gen 'moar/src/gen/config.h', 'moar/build/config.h.in', sub {
+    my ($dest, $src) = @_;
 
-@ARGV = qw(build)
-    unless @ARGV;
+};
 
-for (@ARGV) {
-    die "unknown target `$_´"
-        unless exists $targets{$_};
-
-    $targets{$_}->();
-}
+dispatch @ARGV ? @ARGV : qw(build);
